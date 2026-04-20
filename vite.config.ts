6b449e8be562805f563845b8d569d622da2119d6
Ritesh <ritesh2445m@gmail.com>
2026-04-20 17:57:30 +0530

    chore: switch to Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import viteReact from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-  plugins: [tanstackStart(), netlify(), viteReact(), tailwindcss(), tsConfigPaths()],
+  plugins: [tanstackStart(), viteReact(), tailwindcss(), tsConfigPaths()],
   resolve: {
     alias: {
       "@": "/src",
